977ec2e5d115329d7a2eeefc213a7b6a0dba3faa
andytrigg <andy@sloshydog.com>
2014-09-20 00:32:09 +1000

    Ensure the in memory event store applies event specifications correctly.

--- src/main/java/com/sloshydog/eventuate/inmemory/InMemoryEventStore.java
+++ src/main/java/com/sloshydog/eventuate/inmemory/InMemoryEventStore.java
@@ -1,16 +1,19 @@
 package com.sloshydog.eventuate.inmemory;
 
+import com.google.common.base.Predicate;
 import com.sloshydog.eventuate.api.Event;
 import com.sloshydog.eventuate.api.EventSpecification;
 import com.sloshydog.eventuate.api.EventStore;
 import com.sloshydog.eventuate.api.EventStream;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import static com.google.common.collect.FluentIterable.from;
+import static com.google.common.collect.Lists.newArrayList;
+
 public class InMemoryEventStore implements EventStore {
-    private final List<Event> events = new ArrayList<Event>();
+    private final List<Event> events = newArrayList();
 
     @Override
     public void store(Event applicationEvent) {
@@ -18,12 +21,25 @@ public class InMemoryEventStore implements EventStore {
     }
 
     @Override
-    public EventStream getMatching(EventSpecification eventSpecification) {
+    public EventStream getMatching(final EventSpecification eventSpecification) {
         return new EventStream() {
             @Override
             public Iterator<Event> iterator() {
-                return events.iterator();
+                return from(events).filter(new EventSpecificationPredicate(eventSpecification)).iterator();
             }
         };
+    }
+
+    private static class EventSpecificationPredicate implements Predicate<Event> {
+        private final EventSpecification eventSpecification;
+
+        public EventSpecificationPredicate(EventSpecification eventSpecification) {
+            this.eventSpecification = eventSpecification;
+        }
+
+        @Override
+        public boolean apply(Event event) {
+            return eventSpecification.matches(event);
+        }
     }
 }
--- src/test/java/com/sloshydog/eventuate/inmemory/InMemoryEventStoreTest.java
+++ src/test/java/com/sloshydog/eventuate/inmemory/InMemoryEventStoreTest.java
@@ -1,17 +1,15 @@
 package com.sloshydog.eventuate.inmemory;
 
 import com.sloshydog.eventuate.api.Event;
+import com.sloshydog.eventuate.api.EventSpecification;
 import com.sloshydog.eventuate.api.EventStore;
 import com.sloshydog.eventuate.api.EventStream;
-import com.sloshydog.eventuate.impl.Specifications;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
 public class InMemoryEventStoreTest {
 
     @Test
@@ -20,13 +18,62 @@ public class InMemoryEventStoreTest {
     }
 
     @Test
+    public void shouldReturnAnEmptyEventStreamWhenNoEventHasBeenStored() {
+        Event newEvent = mock(Event.class);
+        EventSpecification eventSpecification = mock(EventSpecification.class);
+
+        when(eventSpecification.matches(newEvent)).thenReturn(true);
+
+        InMemoryEventStore eventStore = new InMemoryEventStore();
+        EventStream eventStream = eventStore.getMatching(eventSpecification);
+        assertThat(eventStream).isEmpty();
+    }
+
+    @Test
     public void shouldBeAbleToStoreASingleEventInMemory() {
         Event newEvent = mock(Event.class);
+        EventSpecification eventSpecification = mock(EventSpecification.class);
+
+        when(eventSpecification.matches(newEvent)).thenReturn(true);
 
         InMemoryEventStore eventStore = new InMemoryEventStore();
         eventStore.store(newEvent);
 
-        EventStream eventStream = eventStore.getMatching(Specifications.any());
+        EventStream eventStream = eventStore.getMatching(eventSpecification);
         assertThat(eventStream).hasSize(1);
+    }
+
+    @Test
+    public void shouldNotReturnAnEventStreamContainingEventsThatDoNotMatchTheSpecification() {
+        Event newEvent = mock(Event.class);
+        EventSpecification eventSpecification = mock(EventSpecification.class);
+
+        when(eventSpecification.matches(newEvent)).thenReturn(false);
+
+        InMemoryEventStore eventStore = new InMemoryEventStore();
+        eventStore.store(newEvent);
+
+        EventStream eventStream = eventStore.getMatching(eventSpecification);
+        assertThat(eventStream).hasSize(0);
+    }
+
+    @Test
+    public void shouldOnlyReturnEventsInTheEventStreamThatMatchTheSpecification() {
+        Event eventOne = mock(Event.class);
+        Event eventTwo = mock(Event.class);
+        Event eventThree = mock(Event.class);
+        EventSpecification eventSpecification = mock(EventSpecification.class);
+
+        when(eventSpecification.matches(eventOne)).thenReturn(true);
+        when(eventSpecification.matches(eventTwo)).thenReturn(false);
+        when(eventSpecification.matches(eventThree)).thenReturn(true);
+
+        InMemoryEventStore eventStore = new InMemoryEventStore();
+        eventStore.store(eventOne);
+        eventStore.store(eventTwo);
+        eventStore.store(eventThree);
+
+        EventStream eventStream = eventStore.getMatching(eventSpecification);
+        assertThat(eventStream).containsExactly(eventOne, eventThree);
     }
 }
