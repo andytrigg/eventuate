5cae0b8c6f81280b3a5bc26a023067986c36e7c9
andytrigg <andy@sloshydog.com>
2014-09-20 00:01:21 +1000

    Implement the in memory event store so unit test passes.

--- src/main/java/com/sloshydog/eventuate/inmemory/InMemoryEventStore.java
+++ src/main/java/com/sloshydog/eventuate/inmemory/InMemoryEventStore.java
@@ -5,15 +5,25 @@ import com.sloshydog.eventuate.api.EventSpecification;
 import com.sloshydog.eventuate.api.EventStore;
 import com.sloshydog.eventuate.api.EventStream;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class InMemoryEventStore implements EventStore {
+    private final List<Event> events = new ArrayList<Event>();
 
     @Override
     public void store(Event applicationEvent) {
-
+        events.add(applicationEvent);
     }
 
     @Override
     public EventStream getMatching(EventSpecification eventSpecification) {
-        return null;
+        return new EventStream() {
+            @Override
+            public Iterator<Event> iterator() {
+                return events.iterator();
+            }
+        };
     }
 }
