1aa6b1625a32c606e73a96762cd42022ab6d362d
andytrigg <andy@sloshydog.com>
2014-09-25 21:17:29 +1000

    Implement the deserializing of payloads..

--- eventsourcing-filesystem/src/main/java/com/sloshydog/eventuate/filesystem/PayloadSerializer.java
+++ eventsourcing-filesystem/src/main/java/com/sloshydog/eventuate/filesystem/PayloadSerializer.java
@@ -2,8 +2,10 @@ package com.sloshydog.eventuate.filesystem;
 
 import com.sloshydog.eventuate.api.EventStoreException;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class PayloadSerializer {
@@ -22,4 +24,18 @@ public class PayloadSerializer {
              }
              return new SimpleSerializedPayload(byteArrayOutputStream.toByteArray());
     }
+
+      public Object deserialize(SerializedPayload serializedPayload) {
+          ObjectInputStream ois = null;
+          try {
+              ois = new ObjectInputStream(new ByteArrayInputStream(serializedPayload.getData()));
+              return ois.readObject();
+          } catch (ClassNotFoundException e) {
+              throw new EventStoreException("An error occurred while deserializing: " + e.getMessage(), e);
+          } catch (IOException e) {
+              throw new EventStoreException("An error occurred while deserializing: " + e.getMessage(), e);
+          } finally {
+              IOUtils.closeQuietly(ois);
+          }
+      }
 }
--- eventsourcing-filesystem/src/test/java/com/sloshydog/eventuate/filesystem/PayloadSerializerTest.java
+++ eventsourcing-filesystem/src/test/java/com/sloshydog/eventuate/filesystem/PayloadSerializerTest.java
@@ -14,4 +14,11 @@ public class PayloadSerializerTest {
         assertThat(serialize).isEqualTo(expectedSerializedPayload);
     }
 
+    @Test
+    public void shouldBeAbleToDeserializeAPayloadToItsOriginalObject() {
+        Object result = new PayloadSerializer().deserialize(new SimpleSerializedPayload(new byte[] {-84,-19,0,5,116,0,10,77,121,32,112,97,121,108,111,97,100}));
+
+        assertThat(result).isEqualTo("My payload");
+    }
+
 }
