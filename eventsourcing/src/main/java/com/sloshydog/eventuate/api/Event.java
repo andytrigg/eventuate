5c8a06137bf2453ff225cd433378bac05585b05b
andytrigg <andy@sloshydog.com>
2014-10-01 08:01:01 +1000

    Added some javadoc to the Event interface.

--- eventsourcing/src/main/java/com/sloshydog/eventuate/api/Event.java
+++ eventsourcing/src/main/java/com/sloshydog/eventuate/api/Event.java
@@ -4,13 +4,41 @@ import org.joda.time.DateTime;
 
 import java.io.Serializable;
 
-public interface Event<E> extends Serializable {
+/**
+ * Represents and event that has occurred. The Payload contains any pertinent information that relates to the event that
+ * has occurred. The event should contain any relevant information that other components may require in order to react
+ * to this event.
+ *
+ * @param <T> The type of the payload contained in the Event
+ * @since 1.0
+ */
+public interface Event<T> extends Serializable {
 
+    /**
+     * Returns the identifier of the Aggregate that generated this Event.
+     *
+     * @return the identifier of the Aggregate that generated this Event
+     */
     String getAggregateIdentifier();
 
+    /**
+     * Returns the time stamp of the occurrence of this Event. This is the time that this Event was created.
+     *
+     * @return the time stamp for this Event
+     */
     DateTime getTimeStamp();
 
+    /**
+     * Returns the type of the Aggregate that generated this Event.
+     *
+     * @return the type of the Aggregate that generated this Event
+     */
     String getAggregateType();
 
-    E getPayload();
+    /**
+     * Returns the payload of this Event. The payload is the application-specific information.
+     *
+     * @return the payload of this Event
+     */
+    T getPayload();
 }
