4fc8c9cd9ff0b092287589d4ef6ae8b8355ff314
andytrigg <andy@sloshydog.com>
2014-10-01 08:33:11 +1000

    Some more java doc

--- eventsourcing/src/main/java/com/sloshydog/eventuate/api/EventStore.java
+++ eventsourcing/src/main/java/com/sloshydog/eventuate/api/EventStore.java
@@ -1,7 +1,22 @@
 package com.sloshydog.eventuate.api;
 
+/**
+ * An EventStore provides the mechanism for event storage. Events are read as {@link com.sloshydog.eventuate.api.EventStream}.
+ *
+ * @since 0.1
+ */
 public interface EventStore {
+
+    /**
+     * Store a single Event in the event store.
+     * @param applicationEvent the event to be stored
+     */
     void store(Event applicationEvent);
 
+    /**
+     * Retrieve an EventStream for the Events in the event store that match the event specification provided.
+     * @param eventSpecification
+     * @return
+     */
     EventStream getMatching(EventSpecification eventSpecification);
 }
