9aada6a696c4d313d6a6e9a588d04cfcddc00aaa
andytrigg <andy@sloshydog.com>
2014-10-02 08:05:23 +1000

    EventStream javadoc.

--- eventsourcing/src/main/java/com/sloshydog/eventuate/api/EventStream.java
+++ eventsourcing/src/main/java/com/sloshydog/eventuate/api/EventStream.java
@@ -2,6 +2,12 @@ package com.sloshydog.eventuate.api;
 
 import java.io.Closeable;
 
+/**
+ * The EventStream represents a stream of historical domain events. The order of events in this stream are in actual
+ * chronological order in which the events happened.
+ *
+ * @since 1.0
+ */
 public interface EventStream extends Iterable<Event>, Closeable {
 
 }
